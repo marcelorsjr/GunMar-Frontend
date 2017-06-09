bb5fa8bb420f45ed31c6d4b0c1838095b9f352f9
marcelorsjr <marrsantosjunior@gmail.com>
2017-06-09 19:43:16 -0300

    added infobox

--- results.php
+++ results.php
@@ -3,17 +3,37 @@
 require_once("lib/Template.class.php");
 include('lib/httpful.phar');
  
+
+
 global $tpl;
 $tpl = new Template("results.html");
       
 $tpl->SEARCH_QUERY = $_GET['query'];
 
 
+
+
+
+
 // CRIA OS BLOCOS COM OS RESULTADOS DA PESQUISA
-$tpl->RESULT_TITLE ="Wladmir Cardoso Brandão";
-$tpl->RESULT_URL = "www.wladmirbrandao.com/";
-$tpl->RESULT_DESCRIPTION = "Wladmir Cardoso Brandão, professor e pesquisador em Ciência da Computação e Ciência da Informação. Empreendedor e consultor em Tecnologia da ...";
+$tpl->RESULT_TITLE ="The Office of Barack and Michelle Obama";
+$tpl->RESULT_URL = "https://www.barackobama.com/";
+$tpl->RESULT_DESCRIPTION = "Traduzir esta página
+Welcome to the Office of Barack and Michelle Obama. We Love You Back. Play video. The Office of Barack and Michelle Obama. © 2017 | Legal & Privacy.";
 $tpl->block("BLOCK_RESULT");
+
+$tpl->RESULT_TITLE ="Barack Obama – Wikipédia, a enciclopédia livre";
+$tpl->RESULT_URL = "https://www.barackobama.com/";
+$tpl->RESULT_DESCRIPTION = "Traduzir esta página
+Welcome to the Office of Barack and Michelle Obama. We Love You Back. Play video. The Office of Barack and Michelle Obama. © 2017 | Legal & Privacy.";
+$tpl->block("BLOCK_RESULT");
+
+$tpl->RESULT_TITLE ="Barack Obama - Wikipedia";
+$tpl->RESULT_URL = "https://www.barackobama.com/";
+$tpl->RESULT_DESCRIPTION = "Traduzir esta página
+Welcome to the Office of Barack and Michelle Obama. We Love You Back. Play video. The Office of Barack and Michelle Obama. © 2017 | Legal & Privacy.";
+$tpl->block("BLOCK_RESULT");
+
 //
 
 
